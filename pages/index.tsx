b7c4c0cd1649be85ce34e44e40990bb4c5af551d
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-01-20 16:36:46 +0500

    link component navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 const Home = () => {
-  return <h1>Home Component</h1>;
+  return (
+    <div>
+      <h1>Home Component</h1>
+      <Link href="/blog">Blog</Link>
+      <br />
+      <Link href="/product">Product</Link>
+    </div>
+  );
 };
 
 export default Home;
--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -1,9 +1,23 @@
-const ProductList = () => {
+import Link from "next/link";
+
+const ProductList = ({ productId = 100 }) => {
   return (
     <>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <Link href="/">Home</Link>
+      <h2>
+        <Link href="/product/1">Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="/product/2">Product 2</Link>
+      </h2>
+      <h2>
+        <Link href="/product/3" replace>
+          Product 3
+        </Link>
+      </h2>
+      <h2>
+        <Link href={`/product/${productId}`}>Product {productId}</Link>
+      </h2>
     </>
   );
 };
