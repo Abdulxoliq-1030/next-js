820e05776df4280fc96edae5b7ee24d24eb3eff4
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-01-20 16:53:59 +0500

    navigation programmatically

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Home = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log("Placing your order");
+    router.replace("/product"); // bu esa route ni ga o'tib uni almashtirish uchun
+    // router.push("/product"); // bu boshqa route ga o'tish uchun
+  };
   return (
     <div>
       <h1>Home Component</h1>
       <Link href="/blog">Blog</Link>
       <br />
       <Link href="/product">Product</Link>
+      <button onClick={handleClick}>Place Order</button>
     </div>
   );
 };
