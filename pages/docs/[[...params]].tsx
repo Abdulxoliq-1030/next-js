f74a578956f707695c90c3e39cd8254ed09410df
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-01-20 16:18:14 +0500

    catch all routes

--- pages/docs/[[...params]].tsx
+++ pages/docs/[[...params]].tsx
@@ -2,18 +2,20 @@ import { useRouter } from "next/router";
 
 const Doc = () => {
   const router = useRouter();
-  const { params } = router.query;
+  const { params = [] } = router.query;
   console.log(params);
-  if (params?.length === 2) {
+
+  if (params.length === 2) {
     return (
       <h1>
         Viewing docs for feature {params[0]} and concept {params[1]}
       </h1>
     );
-  } else if (params?.length === 1) {
+  } else if (params.length === 1) {
     return <h1>Viewing docs for feature {params[0]}</h1>;
   }
-  return <h1>Docs Home Page</h1>;
+
+  return <h1>Doc Page</h1>;
 };
 
 export default Doc;
