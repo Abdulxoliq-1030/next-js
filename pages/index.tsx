0949abfcd9d449d680c3d5adf595c081610d72b3
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-01-20 19:12:58 +0500

    static generation and data fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
 import Link from "next/link";
+import axios from "axios";
 import { useRouter } from "next/router";
 
 const Home = () => {
   const router = useRouter();
+
+  const userService = async () => {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    return data;
+  };
 
   const handleClick = () => {
     console.log("Placing your order");
     router.replace("/product"); // bu esa route ni ga o'tib uni almashtirish uchun
     // router.push("/product"); // bu boshqa route ga o'tish uchun
   };
+
   return (
     <div>
       <h1>Home Component</h1>
@@ -16,6 +25,7 @@ const Home = () => {
       <br />
       <Link href="/product">Product</Link>
       <button onClick={handleClick}>Place Order</button>
+      <button onClick={() => userService()}>CLicked</button>
     </div>
   );
 };
